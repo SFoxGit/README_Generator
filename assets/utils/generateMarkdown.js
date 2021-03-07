f4edfff77e195f66b5e62f48a1b29fb84b235abe
Shawn Fox <foxeasttrojan06@gmail.com>
2021-03-07 00:58:40 -0500

    commented out unused function

--- assets/utils/generateMarkdown.js
+++ assets/utils/generateMarkdown.js
@@ -38,13 +38,10 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license) {
-    return licenseBadge, licenseLink
-  } else {
-    return ''
-  }
-}
+// decided to do this a little differently since I added a bunch of features
+// function renderLicenseSection(license) {
+//   
+// }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
